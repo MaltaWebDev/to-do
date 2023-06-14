695c2058c256590db7a09201aa1d75d320c4fcc7
MaltaWebDev <MaltaWebDev@gmail.com>
2023-06-14 06:31:20 +0200

    Style delete buttons, add js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btn = document.getElementById("btn");
 const todosDiv = document.getElementsByClassName("todos")[0];
+const input = document.getElementById("todo");
 
 btn.addEventListener("click", () => {
-  console.log("clicked");
+  // console.log("clicked");
+  // console.log(input.value);
   const newTodo = document.createElement("p");
-  const todo = document.createTextNode("test entry successful!");
+  const todo = document.createTextNode(input.value);
   const newButton = document.createElement("button");
-  const btnText = document.createTextNode("DEL");
+  const btnText = document.createTextNode("delete");
   newTodo.appendChild(todo);
   newButton.appendChild(btnText);
   todosDiv.appendChild(newTodo);
