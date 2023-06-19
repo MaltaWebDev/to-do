2d8c210d8b98e36ae4b0151df4f058a94eeb7e3d
MaltaWebDev <MaltaWebDev@gmail.com>
2023-06-19 07:57:54 +0200

    User requests to keep state on refresh
    - create function to save state in local storage
    - create function to load state
    - add fn declaration to replace callback function from evt listener
    - call create and save fns from within evt listener
    - call save fn after a todo is  deleted
    - call load fn at end of program to load todos on refresh

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+// initialise variables
 const btn = document.getElementById("btn");
 const todosDiv = document.getElementById("todos");
 const input = document.getElementById("todo");
 
-btn.addEventListener("click", () => {
+// save existing todos to local storage
+function saveToLocalStorage() {
+  const allToDos = Array.from(todosDiv.children).map((todosDiv) => {
+    return todosDiv.textContent.slice(0, -"Delete".length);
+  });
+  localStorage.setItem("todos", JSON.stringify(allToDos));
+}
+
+// get all todos from local storage
+function loadFromLocalStorage() {
+  const savedToDos = localStorage.getItem("todos");
+
+  if (savedToDos) {
+    JSON.parse(savedToDos).forEach((savedToDo) => {
+      createAndAppendTodo(savedToDo);
+    });
+  }
+}
+
+// create new todo and append to todosDiv
+function createAndAppendTodo(todoText) {
   const newToDoDiv = document.createElement("div");
   const newParagraph = document.createElement("p");
-  const todoText = document.createTextNode(input.value);
+  const todoTextNode = document.createTextNode(todoText);
   const newButton = document.createElement("button");
 
-  newParagraph.appendChild(todoText);
+  newParagraph.appendChild(todoTextNode);
   newButton.textContent = "Delete";
   newButton.className = "delete";
 
@@ -16,11 +37,22 @@ btn.addEventListener("click", () => {
   newToDoDiv.appendChild(newButton);
 
   todosDiv.appendChild(newToDoDiv);
+}
+
+// On click, create and append todo, add to local storage
+btn.addEventListener("click", () => {
+  const newToDoText = input.value;
+  createAndAppendTodo(newToDoText);
+  saveToLocalStorage();
   input.value = "";
 });
 
+// event delegation
 todosDiv.addEventListener("click", (evt) => {
   if (evt.target.className === "delete") {
     evt.target.parentElement.remove();
+    saveToLocalStorage();
   }
 });
+
+loadFromLocalStorage();
